570f4af44bde518f2e05dafaaac4e45997204143
hejiayi <janreyho@gmail.com>
2016-08-10 12:34:41 +0800

    add eve_swagger to run.py

--- run.py
+++ run.py
@@ -17,6 +17,7 @@
 
 import os
 from eve import Eve
+from eve_swagger import swagger
 
 # Heroku support: bind to PORT if defined, otherwise default to 5000.
 if 'PORT' in os.environ:
@@ -29,7 +30,26 @@ else:
     host = '127.0.0.1'
 
 app = Eve()
+app.register_blueprint(swagger)
 
+# required. See http://swagger.io/specification/#infoObject for details.
+app.config['SWAGGER_INFO'] = {
+    'title': 'My Supercool API',
+    'version': '1.0',
+    'description': 'an API description',
+    'termsOfService': 'my terms of service',
+    'contact': {
+        'name': 'nicola',
+        'url': 'http://nicolaiarocci.com'
+    },
+    'license': {
+        'name': 'BSD',
+        'url': 'https://github.com/nicolaiarocci/eve-swagger/blob/master/LICENSE',
+    }
+}
+
+# optional. Will use flask.request.host if missing.
+app.config['SWAGGER_HOST'] = 'myhost.com'
 
 if __name__ == '__main__':
     app.run(host=host, port=port)
