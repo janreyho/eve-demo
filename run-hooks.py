2d25077115a67ed4dea80daf550aeed180979dd0
hejiayi <janreyho@gmail.com>
2016-08-13 11:25:01 +0800

    app.on_pre_GET_students +=

--- run-hooks.py
+++ run-hooks.py
@@ -19,11 +19,28 @@ from eve import Eve
 
 
 def piterpy(endpoint, response):
+    print "hejiayi"
+    print endpoint
+    print response
     for document in response['_items']:
         document['PITERPY'] = 'IS SO COOL!'
 
+def post_get_callback(resource, request, payload):
+    print 'A GET on the "%s" endpoint was just performed!' % resource
+
+
+def pre_contacts_get_callback(request, payload):
+    print 'A pre get on "contacts" was just performed!'
+    abort(403)
+
+def post_contacts_get_callback(request, payload):
+    print 'A post get on "contacts" was just performed!'
+
 app = Eve()
-app.on_fetched_resource += piterpy
+# app.on_fetched_resource += piterpy
+# app.on_post_GET += post_get_callback
+app.on_pre_GET_students += pre_contacts_get_callback
+# app.on_post_GET_students += post_contacts_get_callback
 
 if __name__ == '__main__':
     app.run()
